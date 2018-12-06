05884de042c0ee1848625f87d5f8b5a87ff6f01f
Ticketmatic <info@ticketmatic.com>
2018-12-06 12:34:27 +0000

    TMC-15690 Edit properties api example

--- ticketmatic/client.go
+++ ticketmatic/client.go
@@ -24,7 +24,7 @@ var Server = "https://apps.ticketmatic.com"
 var Version = "1"
 
 // Library Version
-const Build = "1.0.108"
+const Build = "1.0.109"
 
 // Rate limit error
 type RateLimitError struct {
